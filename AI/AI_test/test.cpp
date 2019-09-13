b3d2de829d3a2ab28a4b7a98a6bcca8e9d9bf827
Heso113 <hampe_olsson91@hotmail.com>
2019-09-13 13:42:03 +0200

    Added tests to GridFunctions: InitGrid and CreateDebugSystems.

--- AI/AI_test/test.cpp
+++ AI/AI_test/test.cpp
@@ -2,13 +2,37 @@
 #include <GridFunctions.h>
 
 TEST(GridFunctions, InitGrid) {
+
+	unsigned int count = 50 * 50;
+
+	ecs::CompTypeMemDesc types[] = {
+		{ TileComponent::typeID, TileComponent::size, count},
+		{ PositionComponent::typeID, PositionComponent::size, count},
+	};
+	ecs::ECSDesc desc;
+	desc.compTypeCount = 2;
+	desc.compTypeMemDescs = types;
+	desc.systemLayerCount = 10;
   
-  ecs::EntityComponentSystem mEcs;
-  GridFunctions::InitGrid(mEcs, 50, 50, 4);  
-  GridFunctions::CreateDebugSystems(mEcs);
+	ecs::EntityComponentSystem mEcs;
+	mEcs.initialize(desc);
 
-  mEcs.update(0);
+    GridFunctions::InitGrid(mEcs, 50, 50, 4);  
+ 
+	int number_of_tile_components = mEcs.getComponentCountOfType(ecs::components::TileComponent::typeID);
+	int number_of_position_componenets = mEcs.getComponentCountOfType(ecs::components::PositionComponent::typeID);
 
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+    EXPECT_EQ(number_of_position_componenets + number_of_tile_components, count * 2);
+	EXPECT_EQ(number_of_position_componenets, count);
+	EXPECT_EQ(number_of_tile_components, count);
+} 
+
+TEST(GridFunctions, CreateDebugSystems) {
+	ecs::EntityComponentSystem mEcs;
+	GridFunctions::CreateDebugSystems(mEcs);	
+
+	int number_of_systems = mEcs.getTotalSystemCount();
+
+	EXPECT_EQ(number_of_systems, 1);
 }
+
