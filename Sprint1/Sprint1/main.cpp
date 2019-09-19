0a3ef1a2f3bf982796172b201f32298d58d9eeae
Mabaria <magnomrikard@gmail.com>
2019-09-19 14:27:47 +0200

    Updated to new engine standard, added dudeGuy

--- Sprint1/Sprint1/main.cpp
+++ Sprint1/Sprint1/main.cpp
@@ -3,6 +3,7 @@
 #include <DeviceInterface.h>
 #include <UtilityFunctions.h>
 #include <CameraFunctions.h>
+#include <GridFunctions.h>
 #include <DebugInfo.h>
 #include "Shaders.h"
 
@@ -18,7 +19,18 @@ namespace DInfo
 using namespace graphics;
 int main()
 {
+	ecs::CompTypeMemDesc ecsMemDesc[] = {
+		{ TileComponent::typeID, TileComponent::size, 1024 },
+		{ TransformComponent::typeID, TransformComponent::size, 1024 },
+	};
+
+	ecs::ECSDesc ecsDesc;
+	ecsDesc.compTypeCount = 2;
+	ecsDesc.compTypeMemDescs = ecsMemDesc;
+	ecsDesc.systemLayerCount = 10;
+
 	ecs::EntityComponentSystem ecs;
+	ecs.initialize(ecsDesc);
 	CameraFunctions::CreateDevCamera(ecs);
 	CameraFunctions::CreateCameraSystems(ecs);
 
@@ -41,10 +53,12 @@ int main()
 	BufferRegion hexMeshBuffReg, dudeMeshBuffReg;
 
 	CreateDeviceInterface(&pDevice);
-	RenderContext* pContext = pDevice->QueryRenderContext();
-	Texture2DView* pBackBuffer = pDevice->QueryBackBuffer();
+	RenderContext* pContext = pDevice->GetRenderContext();
+	RenderTarget backBuffer;
+	DepthBuffer depthBuffer;
+	pDevice->CreateDepthBuffer(1280, 720, &depthBuffer);
 
-	pDevice->CreatePresentWindow(1280, 720, "D3D11", &pWindow);
+	pDevice->CreatePresentWindow(1280, 720, "D3D11", &backBuffer, &pWindow);
 	
 	pDevice->CreatePipeline(gVertexShader, gPixelShader, &pPipeline);
 	pDevice->CreatePipeline(gVertexShader2, gPixelShader2, &pPipeline2);
@@ -66,21 +80,21 @@ int main()
 
 	ModelLoader::Mesh dudeMesh;
 	hr = dudeMesh.LoadFBX("Models\\dudeTest2Triangulated.fbx");
-	pVertices = dudeMesh.GetVertexPositionVector();
+	std::vector<DirectX::XMFLOAT3>* pVertices2 = dudeMesh.GetVertexPositionVector();
 	if (!dudeMesh.HasUVs())
 	{
-		pDevice->CreateMeshRegion(pVertices->size(), pVertices->data(), dudeMesh.GetNormalVector()->data(), NULL, &dudeMeshBuffReg);
+		pDevice->CreateMeshRegion(pVertices2->size(), pVertices2->data(), dudeMesh.GetNormalVector()->data(), NULL, &dudeMeshBuffReg);
 	}
 	else
 	{
-		pDevice->CreateMeshRegion(pVertices->size(), pVertices->data(), dudeMesh.GetNormalVector()->data(), dudeMesh.GetUVVector()->data(), &dudeMeshBuffReg);
+		pDevice->CreateMeshRegion(pVertices2->size(), pVertices2->data(), dudeMesh.GetNormalVector()->data(), dudeMesh.GetUVVector()->data(), &dudeMeshBuffReg);
 	}
 
 
 	pContext->UploadMeshesToGPU();
 
 	BufferRegion worldMatrixBufferRegion;
-	pDevice->CreateDynamicBufferRegion(sizeof(DirectX::XMFLOAT4X4) * 1024, &worldMatrixBufferRegion);
+	pDevice->CreateDynamicBufferRegion(sizeof(DirectX::XMFLOAT4X4) * 1024, NULL, &worldMatrixBufferRegion);
 	
 	
 	DirectX::XMFLOAT4X4 world(1.0f, 0.0f, 0.0f, 0.0f,
@@ -88,29 +102,49 @@ int main()
 							  0.0f, 0.0f, 1.0f, 0.0f,
 							  0.0f, 0.0f, 0.0f, 1.0f);
 	DirectX::XMStoreFloat4x4(&world,
-		DirectX::XMMatrixMultiply(DirectX::XMMatrixScaling(2.0f, 2.0f, 2.0f), DirectX::XMMatrixRotationRollPitchYaw(-1.5708f, 0.0f, 0.0f)));
+		DirectX::XMMatrixMultiply(DirectX::XMMatrixScaling(2.0f, 2.0f, 2.0f), DirectX::XMMatrixRotationRollPitchYaw(-1.5708f, 0.0f, -1.5708f)));
 
 	DirectX::XMFLOAT4X4 worldMatrices[1024];
-	worldMatrices[0] = world;
+	
+	GridFunctions::CreateGrid(ecs, 32, 32, 2.0f);
+	ecs::ComponentIterator comp_iter = ecs.getAllComponentsOfType(ecs::components::TileComponent::typeID);
+	ecs::BaseComponent* p;
+	ecs::components::TransformComponent* pTransform;
+	DirectX::XMFLOAT3 tile_positions[1024];
+	int counter = 0;
+	while (p = comp_iter.next())
+	{
+		pTransform = static_cast<ecs::components::TransformComponent*>(ecs.getComponentFromEntity(ecs::components::TransformComponent::typeID, p->getEntityID()));
+		tile_positions[counter] = pTransform->position;
+		counter++;
+	}
+	
 	for (int i = 0; i < 32; ++i)
 	{
 		for (int j = 0; j < 32; ++j)
 		{
-			if(i % 2 == 0)
+			if (i % 2 == 0) {
+
 				DirectX::XMStoreFloat4x4(&worldMatrices[i* 32 + j], DirectX::XMMatrixMultiply(DirectX::XMLoadFloat4x4(&world), DirectX::XMMatrixTranslation(j * 3.2f, 0.0f, i * 3.2f)));
-			else
+
+			}
+			else {
 				DirectX::XMStoreFloat4x4(&worldMatrices[i * 32 + j], DirectX::XMMatrixMultiply(DirectX::XMLoadFloat4x4(&world), DirectX::XMMatrixTranslation(j * 3.2f + 1.5f, 0.0f, i * 3.1f)));
+			}
+			DirectX::XMStoreFloat4x4(&worldMatrices[i * 32 + j], DirectX::XMMatrixMultiply(DirectX::XMLoadFloat4x4(&world), DirectX::XMMatrixTranslation(tile_positions[i * 32 + j].x, tile_positions[i * 32 + j].y, tile_positions[i * 32 + j].z)));
 		}
 	}
 
+	BufferRegion dudeMatrixRegion;
 	DirectX::XMFLOAT4X4 dudeMatrix;
-	pDevice->CreateDynamicBufferRegion(sizeof(DirectX::XMFLOAT4X4), &dudeMeshBuffReg);
-	DirectX::XMStoreFloat4x4(&dudeMatrix, DirectX::XMMatrixScaling(3.0f, 3.0f, 3.0f));
+	pDevice->CreateDynamicBufferRegion(sizeof(DirectX::XMFLOAT4X4), NULL, &dudeMatrixRegion);
+	DirectX::XMStoreFloat4x4(&dudeMatrix, DirectX::XMMatrixMultiply(DirectX::XMMatrixTranslation(0.0f, 0.0f, 0.0f),
+		DirectX::XMMatrixMultiply(DirectX::XMMatrixScaling(5.0f, 5.0f, 5.0f), DirectX::XMMatrixRotationRollPitchYaw(-1.5708f, 0.0f, 0.0f))));
 
 	DirectX::XMFLOAT4X4 view;
 	DirectX::XMStoreFloat4x4(&view,
 		DirectX::XMMatrixLookAtLH(
-			{ 32.0f, 20.0f, -20.0f },
+			{ -5.0f, 20.0f, -5.0f },
 			{ 32.0f, 0.0f, 32.0f },
 			{ 0.0f, 1.0f, 0.0f }));
 	DirectX::XMFLOAT4X4 projection;
@@ -124,10 +158,12 @@ int main()
 	// Per Frame Heap
 	pDevice->CreateDynamicBufferRegion(
 		sizeof(view),
+		NULL,
 		&viewRegion);
 
 	pDevice->CreateStaticBufferRegion(
 		sizeof(projection),
+		NULL,
 		&projRegion);
 
 	pContext->VSSetConstantBuffer(1, viewRegion);
@@ -145,6 +181,7 @@ int main()
 
 	pDevice->CreateIndexBufferRegion(
 		sizeof(int) * hexMesh.GetIndexVector()->size(),
+		NULL,
 		&indexRegion
 	);
 	pContext->CopyDataToRegion(
@@ -157,6 +194,7 @@ int main()
 	indices = dudeMesh.GetIndexVector()->data();
 	pDevice->CreateIndexBufferRegion(
 		sizeof(int)* dudeMesh.GetIndexVector()->size(),
+		NULL,
 		&dudeMeshIndexRegion
 	);
 	pContext->CopyDataToRegion(
@@ -164,8 +202,8 @@ int main()
 		sizeof(int)* dudeMesh.GetIndexVector()->size(),
 		dudeMeshIndexRegion);
 
-	pContext->UploadToGPU(BUFFER_CONSTANT_STATIC);
-	pContext->UploadToGPU(BUFFER_INDEX);
+	pContext->UploadStaticDataToGPU();
+	pContext->UploadIndexBuffersToGPU();
 
 	pWindow->Show();
 
@@ -179,35 +217,41 @@ int main()
 		sizeof(DirectX::XMFLOAT4X4) * 1024,
 		worldMatrixBufferRegion);
 
-
+	float rotation = 0.0f;
+	
 	pContext->CopyDataToRegion(
 		&dudeMatrix,
 		sizeof(DirectX::XMFLOAT4X4),
-		dudeMeshBuffReg);
+		dudeMatrixRegion);
 	
 	while (pWindow->IsOpen())
 	{
 		if (!pWindow->Update())
 		{
-
+			DirectX::XMStoreFloat4x4(&dudeMatrix, DirectX::XMMatrixMultiply(DirectX::XMLoadFloat4x4(&dudeMatrix), DirectX::XMMatrixTranslation(rotation / 10.0f, rotation / 20.0f, rotation / 10.0f)));
+			pContext->CopyDataToRegion(
+				&dudeMatrix,
+				sizeof(DirectX::XMFLOAT4X4),
+				dudeMatrixRegion);
 			QueryPerformanceCounter(&starting_time);
 
 			// -----------------------------------------
-			pContext->ClearRenderTarget(pBackBuffer, 34.0f / 255.f, 128.0f / 255.f, 178.0f / 255.f);
-			pContext->SetRenderTarget(pBackBuffer);
+			pContext->ClearRenderTarget(backBuffer, 34.0f / 255.f, 128.0f / 255.f, 178.0f / 255.f);
+			pContext->ClearDepth(depthBuffer);
+			pContext->SetRenderTarget(backBuffer, depthBuffer);
 
 			pContext->CopyDataToRegion(
 				&view,
 				sizeof(view),
 				viewRegion);
-			pContext->UploadToGPU(BUFFER_CONSTANT_DYNAMIC);
+			pContext->UploadDynamicDataToGPU();
 
 			pContext->SetGraphicsPipeline(pPipeline);
 			pContext->VSSetConstantBuffer(0, worldMatrixBufferRegion);
 			pContext->DrawIndexedInstance(1024, 0, indexRegion, hexMeshBuffReg);
 
 			pContext->SetGraphicsPipeline(pPipeline2);
-			pContext->VSSetConstantBuffer(0, dudeMeshBuffReg);
+			pContext->VSSetConstantBuffer(0, dudeMatrixRegion);
 			pContext->DrawIndexedInstance(1, 0, dudeMeshIndexRegion, dudeMeshBuffReg);
 			pWindow->Present();
 			// -----------------------------------------
@@ -216,10 +260,11 @@ int main()
 			elapsed_microseconds.QuadPart *= 1000000;
 			elapsed_microseconds.QuadPart /= freq.QuadPart;
 			ecs.update((float)(elapsed_microseconds.QuadPart) / 1000000.0f);
+			rotation += elapsed_microseconds.QuadPart / 1000000.0f;
 		}
 
 	}
 	pDevice->DeletePipeline(pPipeline);
-	pDevice->Release();
+	graphics::DeleteDeviceInterface(pDevice);
 	
 }
