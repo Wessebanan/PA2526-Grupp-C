d27e8df4e95945f9724ba33cfd282cd73e6b5d04
taumic7 <protium@hotmail.com>
2019-09-26 10:45:56 +0200

    created AIglobals.h to use for things like enum and strucs that can be used in more AI files, Updated AI components to have some info about tile types and navigation

--- AI/AI/includes/AIComponents.h
+++ AI/AI/includes/AIComponents.h
@@ -1,18 +1,20 @@
 #pragma once
 
 #include "ecsComponentIncludes.h"
+#include "AIGlobals.h"
 
 namespace ecs
 {
 	namespace components
 	{
-		struct TileComponent : public ECSComponent<TileComponent>
+		struct TileComponent : public ECSComponent<TileComponent> //Component used for AI pathfinding
 		{
 			/*
 			  This component is used to help filter for tile entities in systems.
 			  It will be expanded on when pathfinding and other systems is implemented.
 			*/
-			int data = 0; // ELLER ENTITY ID
+			TileType tileType = Grass;
+			bool impassable = false;
 		};
 	}
 }
---
+++ AI/AI/includes/AIGlobals.h
@@ -0,0 +1,12 @@
+#pragma once
+
+using TileType = unsigned int;
+enum TileTypes //enum used in AIcomponents right now 
+{
+	Grass,
+	Stone,
+	Water,
+
+	TileTypeCount,
+	Undefined
+};
