e60092e4eaf52b936ee1c2ae71ea72bf60bf1904
Heso113 <hampe_olsson91@hotmail.com>
2019-09-26 15:42:37 +0200

    oof

--- AI/AI_test/test.cpp
+++ AI/AI_test/test.cpp
@@ -1,14 +1,12 @@
 #include "pch.h"
-#include "GridFunctions.h"
-#include "AISystems.h"
+#include <GridFunctions.h>
 
 TEST(GridFunctions, InitGrid) {
 
-	int nr_of_rows = 50;
-	int nr_of_columns = 50;
+	int nr_of_rows = 12;
+	int nr_of_columns = 12;
 	float radius = 4.0f;
 	unsigned int count = nr_of_columns * nr_of_rows;
-
 	//Define some ECS stuff to allow the ECS to create more than 100 of each component.
 	ecs::CompTypeMemDesc types[] = {
 		{ TileComponent::typeID, TileComponent::size, count},
@@ -23,10 +21,10 @@ TEST(GridFunctions, InitGrid) {
 
 
 	//Call function that will create the components of the grid.
-    GridFunctions::CreateGrid(mEcs, nr_of_rows, nr_of_columns, radius);  
+	GridFunctions::CreateGrid(mEcs, nr_of_rows, nr_of_columns, radius);
 	//Fetch the number of components created of each type.
-	size_t number_of_tile_components = mEcs.getComponentCountOfType(ecs::components::TileComponent::typeID);
-	size_t number_of_transform_componenets = mEcs.getComponentCountOfType(ecs::components::TransformComponent::typeID);
+	int number_of_tile_components = mEcs.getComponentCountOfType(ecs::components::TileComponent::typeID);
+	int number_of_transform_componenets = mEcs.getComponentCountOfType(ecs::components::TransformComponent::typeID);
 
 	//Check so that the correct number of components was created.
     EXPECT_EQ(number_of_transform_componenets + number_of_tile_components, count * 2);
@@ -38,33 +36,46 @@ TEST(GridFunctions, CreateDebugSystems) {
 	ecs::EntityComponentSystem mEcs;
 	GridFunctions::CreateDebugSystems(mEcs);	
 
-	size_t number_of_systems = mEcs.getTotalSystemCount();
+	int number_of_systems = mEcs.getTotalSystemCount();
 
 	//Check so that the debug system was created.
 	EXPECT_EQ(number_of_systems, 1);
 }
 
-TEST(AI, CreateAIComponents) {
-	ecs::EntityComponentSystem my_ecs;
-	ecs::components::PathfindingStateComponent psc;
-	ecs::components::MoveStateComponent msc;
-	ecs::components::IdleStateComponent isc;
+TEST(GridFunctions, heightMapTest) {
+	int nr_of_rows = 12;
+	int nr_of_columns = 12;
+	float radius = 4.0f;
+	unsigned int count = nr_of_columns * nr_of_rows;
+	//Define some ECS stuff to allow the ECS to create more than 100 of each component.
+	ecs::CompTypeMemDesc types[] = {
+		{ TileComponent::typeID, TileComponent::size, count},
+		{ TransformComponent::typeID, TransformComponent::size, count},
+	};
+	ecs::ECSDesc desc;
+	desc.compTypeCount = 2;
+	desc.compTypeMemDescs = types;
+	desc.systemLayerCount = 10;
+	ecs::EntityComponentSystem mEcs;
+	mEcs.initialize(desc);
 
-	my_ecs.createEntity(psc, msc, isc);
-	size_t number_of_components = my_ecs.getTotalComponentCount();
-
-	//Check so that the debug system was created.
-	EXPECT_EQ(number_of_components, 3);
+	bool found = false;
+	int nrOfComponents = 0;
+	GridFunctions::CreateGrid(mEcs, nr_of_rows, nr_of_columns, radius); //Create grid with diffrent height
+	ecs::ComponentIterator it = mEcs.getAllComponentsOfType(ecs::components::TransformComponent::typeID); //iterator for all transform components
+	ecs::BaseComponent *p_base;
+	ecs::components::TransformComponent *p_transform;
+	while (p_base = it.next()) //loop through all components and returns a base component
+	{
+		p_transform = (ecs::components::TransformComponent*)p_base; //casts base component to transform component
+		if (p_transform->position.y == 3.f) // check if there is a component with a y pos that is 3(there is only one that has 3 according to the height map)
+		{
+			found = true;
+			nrOfComponents++;
+		}
+	}
+	
+	EXPECT_EQ(found, true);
+	EXPECT_EQ(nrOfComponents, 1);
 }
 
-TEST(AI, CreateAISystems) {
-	ecs::EntityComponentSystem my_ecs;
-	my_ecs.createSystem<ecs::systems::PathfindingStateSystem>();
-	my_ecs.createSystem<ecs::systems::IdleStateSystem>();
-	my_ecs.createSystem<ecs::systems::MoveStateSystem>();
-
-	size_t number_of_systems = my_ecs.getTotalSystemCount();
-
-	//Check so that the debug system was created.
-	EXPECT_EQ(number_of_systems, 3);
-}
