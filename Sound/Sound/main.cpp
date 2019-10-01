92121ab20a01a760c1195d8086b7de83adaefaac
Your Name <you@example.com>
2019-10-01 13:36:41 +0200

    Excluded the main.cpp file

--- Sound/Sound/main.cpp
+++ Sound/Sound/main.cpp
@@ -1,17 +1,4 @@
-#include "SoundEngine.h"
-
 int main()
 {
-	Sound::Engine engine;
-	Sound::PaHandler pa_init;
-	
-	engine.OpenStream();
-	engine.StartStream();
-	engine.StartWorkThread();
-	Pa_Sleep(5000);
-	engine.JoinWorkThread();
-	engine.StopStream();
-	engine.CloseStream();
-
 	return 0;
 }
