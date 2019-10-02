bd668db4f7d718a4084ef9a0c86298bbc600c627
Lhure <maxjonilsson@gmail.com>
2019-10-02 09:32:34 +0200

    Revert "moved script to a seperate file and added send button"
    This reverts commit 45681a989a49848a297ab987342de124d7ec866f.

--- InputHandler/InputHandler/scripts/script.js
+++ InputHandler/InputHandler/scripts/script.js
@@ -1,125 +1,85 @@
- var socket = new WebSocket("ws://192.168.1.45:80");
+function getName()
+{
+  $.getJSON("json/data.json", function(json) {
+      console.log(json); // this will show the info it in firebug console
+      var stringed = JSON.stringify(json);
+      var response = JSON.parse(stringed);
+      console.log(response.name); // this will show the info it in firebug console
 
-    var selectedTileMsg = "0001100100";
-    var selectedButtonMsg = "0002100";
+  });
+};
+function getData()
+{
+  $.getJSON("json/playerMode.json", function(json) {
+      console.log(json); // this will show the info it in firebug console
+      var stringed = JSON.stringify(json);
+      var response = JSON.parse(stringed);
+      console.log(response[2].Name); // this will show the info it in firebug console
 
-    socket.onopen = function (event)
-    {
-        socket.send("The connection was established");
-    };
+  });
+};
 
-    socket.onmessage = function(e)
-    {
-        var msg = e.data;
 
-        if (msg == "name")
-        {
-            socket.send("My name is Ludvig");
+function myFunction()
+{
+    //getData();
+    document.getElementById("test").innerHTML = "Pressed";
+    document.getElementById("JSONList").innerHTML = "";
+
+    var $JSONList = $('#JSONList');
+    $.ajaxSetup({ cache:false });
+
+    //?" + new Date().getTime()
+    $.getJSON("json/playerMode.json", function(json) {
+        console.log(json); // this will show the info it in firebug console
+        var stringed = JSON.stringify(json);
+        var response = JSON.parse(stringed);
+        //console.log(response); // this will show the info it in firebug console
+
+        $.each(response, function(i, itt){
+
+            $JSONList.append( "</br>" + itt.Name + " has pressed: " + itt.Button + " ");
+        })
+
+    });
+}
+
+
+$(function (){
+    var $JSONList = $('#JSONList');
+
+    $.ajax({
+        type:'GET',
+        url:'json/playerMode.json',
+        success: function(jsonInput){
+            var stringed = JSON.stringify(jsonInput);
+            var response = JSON.parse(stringed);
+            $.each(response, function(i, response){
+
+                $JSONList.append(response+ "</br>");
+            })
+        },
+        error: function() {
+            console.log("Error: Reading JSON data");
         }
-        else if(msg == "start")
-        {
-            console.log("We are wating for the client to start");
+    });
+
+
+
+});
+
+
+var clicks = 0;
+function AddClick()
+{
+
+    $.ajax({
+        url: 'addItem.php',
+        type: "POST",
+        dataType: 'json',
+        data: {functionname: 'addItem', arguments: ["age", ++clicks,'json/data.json']},
+        success: function(data) {
+            $('#JSONList').html(data);
         }
-        else if(msg[0] == 1)
-        {
-            document.getElementById("clientMsg1").innerHTML = msg;
-        }
-        else if(msg[0] == 2)
-        {
-            document.getElementById("clientMsg2").innerHTML = msg;
-        }
-        else if(msg[0] == 3)
-        {
-            document.getElementById("clientMsg3").innerHTML = msg;
-        }
-        else if(msg[0] == 4)
-        {
-            document.getElementById("clientMsg4").innerHTML = msg;
-        }
-        else
-            document.getElementById("clientMsg0").innerHTML = msg;
-    }
-
-    function ping()
-    {
-        socket.send("ping");
-    }
-    function start()
-    {
-        socket.send("start");
-
-        openCommand();
-    }
-
-    function openPrep()
-    {
-
-        document.getElementById('prepField').style.display = "block";
-        document.getElementById('commandField').style.display = "none";
-        document.getElementById('startfield').style.display = "none";
-    }
-
-    function openCommand()
-    {
-        document.getElementById('prepField').style.display = "none";
-        document.getElementById('commandField').style.display = "block";
-        document.getElementById('startfield').style.display = "none";
-    }
-
-    function setName(name)
-    {
-        socket.send("0000" +  name.toString());
-    }
-    function setTile(x,y)
-    {
-        var player = "0";
-        var action = "001";
-        x += 100;
-        y += 100;
-        var tileX = x.toString();
-        var tileY = y.toString();
-
-
-        var msg = player.concat(action.concat(tileX.concat(tileY)));
-
-        console.log("Saved tileMsg: " + msg);
-        selectedTileMsg = msg;
-		//socket.send(msg);
-    }
-
-    function setButton(x)
-    {
-        var player = "0";
-        var action = "002";
-        x += 100;
-        var Button = x.toString();
-
-
-        var msg = player.concat(action.concat(Button));
-
-        console.log("Saved buttonMsg: " + msg);
-        selectedTileMsg = msg;
-        //socket.send(msg);
-    }
-
-    function setCommand(buttonCommand)
-    {
-        var player = "0";
-        var action = "003";
-        var Button = buttonCommand.toString();
-
-
-        var msg = player.concat(action.concat(Button));
-
-        console.log(msg);
-        socket.send(msg);
-    }
-
-
-	function sendButtonAndTile()
-	{
-		console.log(selectedButtonMsg);
-		socket.send(selectedButtonMsg);
-		console.log(selectedTileMsg);
-		socket.send(selectedTileMsg);
-	};
+    });
+}
