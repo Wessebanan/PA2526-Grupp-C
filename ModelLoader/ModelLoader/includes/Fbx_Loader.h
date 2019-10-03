14fd83d64f1ea4720e1c17db7a22ca2bec7f24ab
Mabaria <magnomrikard@gmail.com>
2019-10-03 14:25:24 +0200

    Added animation support and new lib

--- ModelLoader/ModelLoader/includes/Fbx_Loader.h
+++ ModelLoader/ModelLoader/includes/Fbx_Loader.h
@@ -23,10 +23,60 @@ namespace ModelLoader
 		float uv[2];
 	};
 
+	//This stores the information of each key frame of each Joint
+	struct KeyFrame {
+		FbxLongLong mFrameNum;
+		FbxAMatrix mGlobalTransform;	//transform matrix
+		FbxAMatrix mLocalTransform;
+		FbxAMatrix mOffsetMatrix;
+		KeyFrame* mNext;
+		KeyFrame() :
+			mNext(nullptr)
+		{}
+	};
+
+	struct Joint {
+		std::string mName;
+		int mParentIndex;	//index to its parent joint
+		KeyFrame* mAnimation;
+		std::vector<KeyFrame> mAnimationVector;
+		FbxNode* mNode;
+
+		FbxAMatrix mGlobalBindposeInverse;
+		FbxAMatrix mBoneGlobalTransform;
+		FbxAMatrix mBoneLocalTransform;
+		FbxAMatrix mOffsetMatrix;
+
+		Joint() :
+			mAnimation(nullptr),
+			mNode(nullptr)
+		{
+			mParentIndex = -1;
+		}
+	};
+
+	struct Skeleton {
+		std::vector<Joint> joints;
+	};
+
+	struct IndexWeightPair
+	{
+		unsigned int index;	//index of joint 
+		double weight;		//weight of influence by the joint
+		IndexWeightPair() :
+			index(0), weight(0.0)
+		{}
+	};
+
+	struct ControlPointInfo
+	{
+		IndexWeightPair weightPairs[4];
+	};
+
 	// Used for loading the very basics of an FBX
 	// Input: std::string file name of FBX file, pointers to std::vectors to append the data to
 	// Output: Appends data to the provided vectors, returns HRESULT
 	HRESULT LoadFBX(const std::string& fileName, std::vector<DirectX::XMFLOAT3>* pOutVertexPosVector, std::vector<int>* pOutIndexVector,
-		std::vector<DirectX::XMFLOAT3>* pOutNormalVector, std::vector<DirectX::XMFLOAT2>* pOutUVVector);
+		std::vector<DirectX::XMFLOAT3>* pOutNormalVector, std::vector<DirectX::XMFLOAT2>* pOutUVVector, Skeleton* pOutSkeleton, std::vector<ControlPointInfo>* pOutCPInfoVector);
 
 }
