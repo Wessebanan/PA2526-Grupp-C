bc9a62df2e901a8427e2cd424641a242fe964ebc
Wessebanan <robin_westerlund@hotmail.com>
2019-10-03 10:33:46 +0200

    Just commiting cause I gotta restart VS.

---
+++ Game/Demo2/InitPhysics.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "ecs.h"
+#include "PhysicsComponents.h"
+#include "CollisionSystem.h"
+#include "MovementSystem.h"
+
+void InitGravityOnUnits(ecs::EntityComponentSystem& rEcs)
+{
+
+}
