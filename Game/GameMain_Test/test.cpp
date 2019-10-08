4396c4cc89cd99896921bd22fc2a61107c638a9e
Wessebanan <robin_westerlund@hotmail.com>
2019-10-08 15:47:04 +0200

    Made a shitty solution for the test project including all physics files since it just would not work if I tried to include headers from another project since that project is not a lib.

--- Game/GameMain_Test/test.cpp
+++ Game/GameMain_Test/test.cpp
@@ -1,11 +1,14 @@
-#include "pch.h"
+//#include "pch.h"
+#include "gtest/gtest.h"
+
 #include "Physics/GroundCollision.h"
 #include "Physics/MovementLogic.h"
 #include "Physics/ObjectCollision.h"
 #include "Mesh.h"
 #include <crtdbg.h>
 
-int main(int argc, char** argv) {
+int main(int argc, char** argv) 
+{
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 	::testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
