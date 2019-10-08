ab88e30aeda69c2d151649f47805c21944af172a
Lhure <maxjonilsson@gmail.com>
2019-10-08 15:10:25 +0200

    now runs without input but with den fina renderingen

--- Game/GameMain/main.cpp
+++ Game/GameMain/main.cpp
@@ -1,5 +1,10 @@
 #include "ecs.h"
-#include "Input/InitInput.h"
+#include "gameRendering/InitMesh.h"
+#include "gameRendering/PlaceMesh.h"
+#include "gameAI/InitArmy.h"
+#include "gameAI/InitGrid.h"
+#include "gameUtility/CameraFunctions.h"
+
 
 using namespace ecs;
 
@@ -7,11 +12,50 @@ int main()
 {
 	EntityComponentSystem ecs;
 
-	initInput(ecs);
+	ecs.reserveComponentCount<ecs::components::TransformComponent>(144 + 12 + 1);
+	ecs.reserveComponentCount<ecs::components::TileComponent>(144);
+
+	
 
 
 
-	ecs.update(0.0f);
+	InitGrid(ecs);
+	InitArmy(ecs);
+	CameraFunctions::CreateDevCamera(ecs);
 
+	
+	rendering::RenderManager mng;
+	mng.Initialize(1600, 900, "D3D11");
+
+
+	InitMesh(mng);
+
+	graphics::PresentWindow* pWnd = mng.GetPresentWindow();
+
+	PlaceMesh(ecs, mng);
+	ecs::ComponentIterator itt = ecs.getAllComponentsOfType(ecs::components::CameraComponent::typeID);
+	components::CameraComponent* camComp;
+	camComp = (components::CameraComponent*)itt.next();
+	mng.SetViewMatrix(camComp->viewMatrix);
+	pWnd->Show();
+
+	ecs.update(0.1f);
+	while (pWnd->IsOpen())
+	{
+		if (!pWnd->Update())
+		{
+			mng.Clear(0.2f, 0.1f, 0.1f);
+				
+			
+			mng.SetViewMatrix(camComp->viewMatrix);
+			
+			mng.Draw();
+			pWnd->Present();
+
+			ecs.update(0.1f);
+		}
+	}
+
+	mng.Destroy();
 	return 0;
 }
