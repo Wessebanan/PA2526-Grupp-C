6ad0a3d2f6540534605a05b81a830ea4d1f8cd05
Marcus Holmberg <mackan.holmberg@hotmail.com>
2019-10-10 16:03:55 +0200

    Added comments.

--- Memory/Memory/includes/FreeListAllocator.h
+++ Memory/Memory/includes/FreeListAllocator.h
@@ -58,13 +58,13 @@ namespace memory
 			void Clear() override;
 
 			/*
-				Reserves a block of memory in the allocator's memory heap, and returns a pointer to it.
+				Reserves a block of memory in the allocator's memory, and returns a pointer to it.
 				Returns nullptr if allocation failed.
 			*/
 			void* Allocate(uint size) override;
 
 			/*
-				Returns a block of reserved memory back to the allocator's memory heap, so that the memory
+				Returns a block of reserved memory back to the allocator's memory, so that the memory
 				can be used for future allocations.
 			*/
 			void Free(void* pObject) override;
@@ -73,6 +73,7 @@ namespace memory
 				Getters
 			*/
 
+			// Returns the size of the header that is allocated for each allocation.
 			inline static uint GetAllocationHeaderSize()
 			{
 				return sizeof(AllocationHeader);
@@ -80,6 +81,11 @@ namespace memory
 
 		private:
 
+			/*
+				As every allocation can differ in size, each allocation need an header that
+				specify the size of the allocation. The size is needed when it's time to free
+				the allocation.
+			*/
 			struct AllocationHeader
 			{
 				AllocationHeader(uint memorySize) : size(memorySize), padding(0) {}
@@ -87,6 +93,11 @@ namespace memory
 				uint padding;
 			};
 
+			/*
+				FreeBlocks are placed at the start of memory that is not reserved, so we know how large
+				that gap of available memory is. FreeBlock also contain a pointer to the next available
+				memory block; creating a linked list of free memory.
+			*/
 			struct FreeBlock
 			{
 				FreeBlock(uint memorySize, FreeBlock* pNextBlock = nullptr) : size(memorySize), pNext(pNextBlock) { }
@@ -94,7 +105,10 @@ namespace memory
 				uint size;
 			};
 
+			// Pointer to the first free memory block. If nullptr, the allocator is either full or not initialized.
 			FreeBlock* mpFreeBlocks;
+
+
 
 			/*
 				Help methods
@@ -114,11 +128,18 @@ namespace memory
 				return (void*)((char*)pHeaderStart + HEADER_SIZE);
 			}
 
+			/*
+				Returns the pointer to the header holds the information about a
+				given allocation.
+			*/
 			inline AllocationHeader* FetchHeader(void* pAllocation)
 			{
 				return (AllocationHeader*)((char*)pAllocation - sizeof(AllocationHeader));
 			}
 
+			/*
+				Returns true if a given allocation is allocated by this allocator.
+			*/
 			inline bool IsAllocatedByMe(void* pAllocation)
 			{
 				const intptr_t START = reinterpret_cast<intptr_t>(mpMemoryStart);
--- Memory/Memory/src/FreeListAllocator.cpp
+++ Memory/Memory/src/FreeListAllocator.cpp
@@ -11,6 +11,13 @@ memory::allocators::FreeListAllocator::~FreeListAllocator()
 	//
 }
 
+/*
+	-- Initialize
+	An allocator can be used when it's given some memory to work with,
+	and the size of that memory.
+	This methods set all necessary data in order to work with the memory, 
+	and calls Clear() in order to initialize the free list.
+*/
 bool memory::allocators::FreeListAllocator::Initialize(void* memoryStart, uint memorySize)
 {
 	mpMemoryStart = memoryStart;
@@ -20,6 +27,11 @@ bool memory::allocators::FreeListAllocator::Initialize(void* memoryStart, uint m
 	return true;
 }
 
+/*
+	-- Terminate
+	Clears all meta data in the allocator, making it unable to allocate until
+	another Initialize() is called.
+*/
 void memory::allocators::FreeListAllocator::Terminate()
 {
 	mpMemoryStart = mpFreeBlocks = nullptr;
@@ -27,14 +39,33 @@ void memory::allocators::FreeListAllocator::Terminate()
 	Allocator::Terminate();
 }
 
+
+/*
+	-- Clear
+	Forces all allocations to be forgotten in the memory, making all of the memory
+	available for allocations.
+*/
 void memory::allocators::FreeListAllocator::Clear()
 {
 	mMemoryUsed = 0;
 	mpFreeBlocks = new(mpMemoryStart) FreeBlock(mMemorySize);
 }
 
+/*
+	-- Allocate
+	Reserves a chunk of memory with a requested size and returns a pointer to it.
+	In order to free the memory later, we allocate some more memory in order to
+	store an allocation header before the actual allocation that is send back that
+	will store information about the allocation.
+
+	In memory, an allocation for an int looks like:
+	|//////| Header(size=4) | int(1337) |/////////////|		(/// = Other allocations)
+	                        ^
+	                Returning pointer
+*/
 void* memory::allocators::FreeListAllocator::Allocate(uint size)
 {
+	// Calculate the needed allocation size, including the header.
 	const uint allocation_size = size + sizeof(AllocationHeader);
 
 	// Sanity check if allocation is possible
@@ -43,12 +74,10 @@ void* memory::allocators::FreeListAllocator::Allocate(uint size)
 		return nullptr;
 	}
 
+	// Create a pointer that will store the returning address
 	void* p_allocation = nullptr;
 
-	// Iterate free blocks until we find a block that fits size
-	FreeBlock *p_block = nullptr, *p_last_block = nullptr;
-
-	p_block = mpFreeBlocks;
+	FreeBlock* p_block = mpFreeBlocks;
 
 	/*
 		First, we check if the first block fits the required size.
@@ -65,13 +94,20 @@ void* memory::allocators::FreeListAllocator::Allocate(uint size)
 	*/
 	if (p_block->size == allocation_size)
 	{
-		// Size of first block is equal to the allocation size
+		/*
+			Size of first block is equal to the allocation size.
+			Pop the first free block from free list.
+		*/
 		mpFreeBlocks = p_block->pNext;
 		p_allocation = MakeAllocationAt(p_block, size);
 	}
 	else if (p_block->size > allocation_size)
 	{
-		// Size of first block is larger than the allocation size
+		/*
+			Size of first block is larger than the allocation size
+			Offset the start of the first free block by the size of an
+			allocation.
+		*/
 
 		// Push start of free block in memory by size
 		void* p_new_free_block_position = (void*)((char*)p_block + allocation_size);
@@ -81,11 +117,24 @@ void* memory::allocators::FreeListAllocator::Allocate(uint size)
 	}
 	else
 	{
-		// First block does not fit the requested size, start iterating the list
+		/*
+			First block does not fit the requested size
+			Start iterating the free list to find the first suitable free block.
+		*/
 
-		p_last_block = p_block;
+		/*
+			Store the last block, as we need to update its pNext when the change
+			the block after it.
+		*/
+		FreeBlock* p_last_block = p_block;
 		p_block = p_block->pNext;
 
+		/*
+			End conditions for loop:
+				1. p_allocation != nullptr
+				2. Stepping past the last free block, e.g. no free block fits the
+				   requested allocation -> return nullptr.
+		*/
 		do
 		{
 			/*
@@ -98,13 +147,25 @@ void* memory::allocators::FreeListAllocator::Allocate(uint size)
 				return nullptr;
 			}
 
+			/*
+				Same as before, except we update p_last_block instead of mpFreeBlocks.
+			*/
 			if (p_block->size == allocation_size)
 			{
+				/*
+					Pop current block from free list.
+					User pointer to current block to place a header.
+				*/
 				p_last_block->pNext = p_block->pNext;
 				p_allocation = MakeAllocationAt(p_block, size);
 			}
 			else if (p_block->size > allocation_size)
-			{		
+			{
+				/*
+					Place allocation header at position of p_block.
+					Create a new FreeBlock right after the allocation.
+					Link together last free block with new free block.
+				*/
 				p_allocation = MakeAllocationAt(p_block, size);
 
 				// Push start of free block in memory by size
@@ -115,12 +176,14 @@ void* memory::allocators::FreeListAllocator::Allocate(uint size)
 		} while (!p_allocation);
 	}
 
-	intptr_t last_address = reinterpret_cast<intptr_t>((void*)((char*)mpMemoryStart + mMemorySize));
-	intptr_t allocation_address = reinterpret_cast<intptr_t>(p_allocation);
-
 	return p_allocation;
 }
 
+/*
+	-- Free
+	Releases the reservation of an allocation, making it free for future allocations.
+	Start with checking if the allocation pointer belogn
+*/
 void memory::allocators::FreeListAllocator::Free(void* pObject)
 {
 	/*
