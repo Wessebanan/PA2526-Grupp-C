82e370423219c537160ab3ce750bab894883f679
Lhure <maxjonilsson@gmail.com>
2019-10-11 09:27:11 +0200

    fixed merg

--- Game/GameMain/gameSceneObjects/InitBiomes.h
+++ Game/GameMain/gameSceneObjects/InitBiomes.h
@@ -25,9 +25,9 @@ void InitBiomes(ecs::EntityComponentSystem& rECS)
 	{
 		for (size_t j = 0; j < 4; j++)
 		{
-			ecs::components::TileComponent* p_tile_comp = rECS.getComponentFromEntity<ecs::components::TileComponent>(p_start[j].Id);
+			ecs::components::TileComponent* p_p_tile_comp = rECS.getComponentFromEntity<ecs::components::TileComponent>(p_start[j].Id);
 
-			p_tile_comp->biome = (BIOME)j;
+			p_p_tile_comp->biome = (BIOME)j;
 
 			switch (j)
 			{
@@ -57,8 +57,8 @@ void InitBiomes(ecs::EntityComponentSystem& rECS)
 
 
 
-		ecs::components::TransformComponent* tile_tansf_comp = rECS.getComponentFromEntity<ecs::components::TransformComponent>(tile_comp->getEntityID());
-		ecs::components::ColorComponent* tile_color_comp = rECS.getComponentFromEntity<ecs::components::ColorComponent>(tile_comp->getEntityID());
+		ecs::components::TransformComponent* p_tile_tansf_comp = rECS.getComponentFromEntity<ecs::components::TransformComponent>(p_tile_comp->getEntityID());
+		ecs::components::ColorComponent* p_tile_color_comp = rECS.getComponentFromEntity<ecs::components::ColorComponent>(p_tile_comp->getEntityID());
 
 		int random = rand() % 50;
 		int color_offset = -25 + random;
@@ -66,24 +66,24 @@ void InitBiomes(ecs::EntityComponentSystem& rECS)
 		switch (p_tile_comp->biome)
 		{
 		case SWAMP:
-			tile_tansf_comp->position.y += 0.1f;
-			tile_tansf_comp->position.y *= 1.2f;
-			tile_color_comp->color = DirectX::XMFLOAT4( 0.0f , 30.0f + color_offset , 30.0f + color_offset, 0.0f );
+			p_tile_tansf_comp->position.y += 0.1f;
+			p_tile_tansf_comp->position.y *= 1.2f;
+			p_tile_color_comp->color = DirectX::XMFLOAT4( 0.0f , 30.0f + color_offset , 30.0f + color_offset, 0.0f );
 			break;
 		case MOUNTAIN:
-			tile_tansf_comp->position.y += 0.4f;
-			tile_tansf_comp->position.y *= 2.2f;
-			tile_color_comp->color = DirectX::XMFLOAT4(50.0f + color_offset, 50.0f + color_offset, 50.0f + color_offset, 0.0f);
+			p_tile_tansf_comp->position.y += 0.4f;
+			p_tile_tansf_comp->position.y *= 2.2f;
+			p_tile_color_comp->color = DirectX::XMFLOAT4(50.0f + color_offset, 50.0f + color_offset, 50.0f + color_offset, 0.0f);
 			break;
 		case FIELD:
-			tile_tansf_comp->position.y += 0.2f;
-			tile_tansf_comp->position.y *= 1.4f;
-			tile_color_comp->color = DirectX::XMFLOAT4(0.0f, 150.0f + color_offset, 0.0f, 0.0f);
+			p_tile_tansf_comp->position.y += 0.2f;
+			p_tile_tansf_comp->position.y *= 1.4f;
+			p_tile_color_comp->color = DirectX::XMFLOAT4(0.0f, 150.0f + color_offset, 0.0f, 0.0f);
 			break;
 		case DESERT:
-			tile_tansf_comp->position.y += 0.1f;
-			tile_tansf_comp->position.y *= 1.1f;
-			tile_color_comp->color = DirectX::XMFLOAT4(100.0f + color_offset, 100.0f + color_offset, 00.0f, 0.0f);
+			p_tile_tansf_comp->position.y += 0.1f;
+			p_tile_tansf_comp->position.y *= 1.1f;
+			p_tile_color_comp->color = DirectX::XMFLOAT4(100.0f + color_offset, 100.0f + color_offset, 00.0f, 0.0f);
 			break;
 		default:
 			break;
