a7768779bce1f2655621d8a2f01536a6c135ce0c
Wessebanan <robin_westerlund@hotmail.com>
2019-10-14 14:20:36 +0200

    Did the task, no tests cause its just cåmpånänts

--- Game/GameMain/Physics/PhysicsEvents.h
+++ Game/GameMain/Physics/PhysicsEvents.h
@@ -7,15 +7,6 @@ namespace ecs
 {
 	namespace events
 	{
-		// MovementInputs are used to move entities in various directions.
-		//enum MovementInputs
-		//{
-		//	FORWARD		= 0, 
-		//	RIGHT		= 90, 
-		//	LEFT		= -90, 
-		//	BACKWARD	= 180
-		//};
-		
 		// This event should be created
 		// whenever a movement action is performed
 		// in order to alert the movement systems.
