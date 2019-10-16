a62e958bed8000c263cd49620482e18ab83a41fd
Mabaria <magnomrikard@gmail.com>
2019-10-16 13:52:39 +0200

    Added new features for animation and skeleton loading to ModelLoader, libs not yet created

--- ModelLoader/ModelLoader/includes/Fbx_Loader.h
+++ ModelLoader/ModelLoader/includes/Fbx_Loader.h
@@ -13,6 +13,8 @@
 #pragma comment (lib, "d3d11.lib")
 #pragma comment (lib, "dxguid.lib")
 
+#define MAX_NUM_WEIGHTS_PER_VERTEX 4
+
 namespace ModelLoader
 {
 
@@ -23,10 +25,61 @@ namespace ModelLoader
 		float uv[2];
 	};
 
+	//This stores the weight of each Control Point
+	struct IndexWeightPair
+	{
+		unsigned int index;	//index of joint 
+		double weight;		//weight of influence by the joint
+		IndexWeightPair() :
+			index(0), weight(0.0)
+		{}
+	};
+
+
+	// Each vertex contains up to four IndexWeightPairs
+	struct ControlPointInfo
+	{
+		IndexWeightPair weightPairs[4];
+	};
+
+	//This stores the information of each key frame of each Joint
+	struct KeyFrame {
+		FbxLongLong mFrameNum;
+		FbxAMatrix mGlobalTransform;	//global transform not including bindpose inverse transform matrix
+		FbxAMatrix mLocalTransform;
+		DirectX::XMFLOAT4X4 mOffsetMatrix; // final offset matrix, this is what needs to be sent to the GPU
+	};
+
+	struct Joint {
+		std::string mName;
+		int mParentIndex;	//index to its parent joint
+		std::vector<KeyFrame> mAnimationVector;
+		FbxNode* mNode;
+
+		std::vector<unsigned int> mConnectedVertexIndices;
+
+		FbxAMatrix mGlobalBindposeInverse;
+		FbxAMatrix mBoneGlobalTransform;
+		FbxAMatrix mOffsetMatrix;
+
+		Joint() :
+			mNode(nullptr)
+		{
+			mParentIndex = -1;
+		}
+	};
+
+	struct Skeleton {
+		std::vector<Joint> joints;
+		DirectX::XMFLOAT4X4* animationData;
+		unsigned int jointCount;
+		unsigned int frameCount;
+	};
+
 	// Used for loading the very basics of an FBX
 	// Input: std::string file name of FBX file, pointers to std::vectors to append the data to
 	// Output: Appends data to the provided vectors, returns HRESULT
 	HRESULT LoadFBX(const std::string& fileName, std::vector<DirectX::XMFLOAT3>* pOutVertexPosVector, std::vector<int>* pOutIndexVector,
-		std::vector<DirectX::XMFLOAT3>* pOutNormalVector, std::vector<DirectX::XMFLOAT2>* pOutUVVector);
+		std::vector<DirectX::XMFLOAT3>* pOutNormalVector, std::vector<DirectX::XMFLOAT2>* pOutUVVector, Skeleton* pOutSkeleton, std::vector<ModelLoader::ControlPointInfo>* pOutCPInfoVector);
 
 }
