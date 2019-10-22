7f8ad79175a824c6c4f0ef0ea20f74353614a946
Lhure <maxjonilsson@gmail.com>
2019-10-22 14:55:32 +0200

    fixes to merg

--- Game/GameMain/gameAI/InitGrid.h
+++ Game/GameMain/gameAI/InitGrid.h
@@ -12,6 +12,6 @@
 
 void InitGrid(ecs::EntityComponentSystem& rECS)
 {
-	GridEcsFunctions::CreateGrid(rECS, 20, 14, 1.0f);
-	InitBiomes(rECS, 20, 14);
+	GridEcsFunctions::CreateGrid(rECS, 24, 22, 1.0f);
+	InitBiomes(rECS, 24, 22);
 }
