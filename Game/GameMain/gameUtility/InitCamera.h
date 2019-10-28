10f7cfdf1baba87a8d018eef990fbc2ef837272f
Mabaria <magnomrikard@gmail.com>
2019-10-28 15:36:23 +0100

    Undo camera release change

--- Game/GameMain/gameUtility/InitCamera.h
+++ Game/GameMain/gameUtility/InitCamera.h
@@ -6,7 +6,7 @@
 void InitCamera(ecs::EntityComponentSystem& rECS)
 {
 	CameraEcsFunctions::CreateDevCamera(rECS);
-//#ifdef _DEBUG
+#ifdef _DEBUG
 	rECS.createSystem<ecs::systems::UpdateCameraSystem>();
-//#endif //_DEBUG
+#endif //_DEBUG
 }
