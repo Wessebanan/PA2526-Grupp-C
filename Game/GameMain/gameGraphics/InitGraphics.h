ffd7211cff4950c53e099ba344897795aae9d032
Filip Lundbeck <craiynel@gmail.com>
2019-10-30 11:36:06 +0100

    updated shadow map

--- Game/GameMain/gameGraphics/InitGraphics.h
+++ Game/GameMain/gameGraphics/InitGraphics.h
@@ -80,10 +80,10 @@ void InitGraphicsComponents(EntityComponentSystem& rEcs, UINT renderBufferSize, 
 	components::PipelineForwardComponent* p_pfComp = rEcs.getComponentFromEntity<components::PipelineForwardComponent>(graphics_entity_id);
 
 	p_psmComp->pipelineDesc.PixelsWidth = 2048;
-	p_psmComp->pipelineDesc.Width = 60.0f;
+	p_psmComp->pipelineDesc.Width = 80.0f;
 	p_psmComp->pipelineDesc.Height = 80.0f;
-	p_psmComp->pipelineDesc.NearPlane = 1.0f;
-	p_psmComp->pipelineDesc.FarPlane = 40.0f;
+	p_psmComp->pipelineDesc.NearPlane = 8.0f;
+	p_psmComp->pipelineDesc.FarPlane = 80.0f;
 	p_psmComp->pipeline = r_renderer_mgr.CreatePipeline(new graphics::ShadowMapPipeline, &p_psmComp->pipelineDesc);
 
 	p_pfComp->pipelineDesc.ClientWidth = clientWidth;
