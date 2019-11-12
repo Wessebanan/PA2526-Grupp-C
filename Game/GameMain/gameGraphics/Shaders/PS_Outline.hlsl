b42f051d74b226d4b3a7a4da859be9e00e31d3c2
Mabaria <magnomrikard@gmail.com>
2019-11-12 15:52:07 +0100

    Add comments

--- Game/GameMain/gameGraphics/Shaders/PS_Outline.hlsl
+++ Game/GameMain/gameGraphics/Shaders/PS_Outline.hlsl
@@ -20,12 +20,17 @@ PSOut PS(VSOut input) : SV_Target
 	PSOut output;
 	int stencil_sum = 0;
 
+	// Accumulate sum of stencil values of neighbouring pixels
+	// Decrement by 1024 if current pixel is stenciled, we don't want to draw outlines inside the objects
 	stencil_sum -= 1024 * StencilTexture.Load(int3(int2(input.Pos.x, input.Pos.y), 0)).y;
 	stencil_sum += StencilTexture.Load(int3(int2(input.Pos.x + OUTLINE_THICKNESS, input.Pos.y + 0.0f), 0)).y;
 	stencil_sum += StencilTexture.Load(int3(int2(input.Pos.x + 0.0f, input.Pos.y + OUTLINE_THICKNESS), 0)).y;
 	stencil_sum += StencilTexture.Load(int3(int2(input.Pos.x - OUTLINE_THICKNESS, input.Pos.y + 0.0f), 0)).y;
 	stencil_sum += StencilTexture.Load(int3(int2(input.Pos.x + 0.0f, input.Pos.y - OUTLINE_THICKNESS), 0)).y;
 
+	// Valid stencil values are 1, 5, 26, 105 that correspond to different colours
+	// Assuming all adjacent pixels are stenciled
+	// Colors should be set to the various army colors
 	float4 finalColor = float4(0.0f, 0.0f, 0.0f, 1.0f);
 	if (stencil_sum >= 105)
 	{
@@ -50,6 +55,7 @@ PSOut PS(VSOut input) : SV_Target
 	if (stencil_sum > 0)
 		return output;
 
+	// Discard pixel if we don't have to draw anything
 	discard;
 	return output;
 }
--- Game/GameMain/gameGraphics/Shaders/VS_Outline.hlsl
+++ Game/GameMain/gameGraphics/Shaders/VS_Outline.hlsl
@@ -8,6 +8,9 @@ struct VSOut
 
 VSOut VS(uint VertexID: SV_VertexID)
 {
+	// This shader assumes an input draw of 3 vertices with no IA, no vertex buffer and no index buffer set
+	// It will create a triangle that covers the entire screen, as seen in the following screenshot
+	// https://i.imgur.com/jE0UlZ4.png
 	VSOut output = (VSOut)0;
 	output.Pos = float4(float2(((VertexID << 1) & 2) * 2.0f, (VertexID == 0) * -4.0f) + float2(-1.0f, 1.0f), 0.0f, 1.0f);
 	return output;
