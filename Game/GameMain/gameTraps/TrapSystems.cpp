bdad07c7d669d476be78bcaa2fd9c0d175e65bb9
Lhure <maxjonilsson@gmail.com>
2019-11-13 13:47:40 +0100

    gotta dip

--- Game/GameMain/gameTraps/TrapSystems.cpp
+++ Game/GameMain/gameTraps/TrapSystems.cpp
@@ -2,6 +2,8 @@
 #include "../Physics/PhysicsComponents.h"
 #include "../Physics/PhysicsEvents.h"
 #include "../gameAI/AIComponents.h"
+#include "../gameUtility/UtilityComponents.h"
+#include "GridProp.h"
 
 ecs::systems::FireTrapEventSystem::FireTrapEventSystem()
 {
@@ -37,5 +39,66 @@ void ecs::systems::FireTrapEventSystem::readEvent(BaseEvent& event, float delta)
 			ecs::components::DeadComponent dead_comp;
 			ecs::ECSUser::createComponent(id, dead_comp);
 		}
+	}
+}
+
+ecs::systems::SpringTrapEventSystem::SpringTrapEventSystem()
+{
+	updateType = EventReader;
+	typeFilter.addRequirement(ecs::events::TriggerSpringTrapEvent::typeID);
+}
+
+ecs::systems::SpringTrapEventSystem::~SpringTrapEventSystem()
+{
+}
+
+void ecs::systems::SpringTrapEventSystem::readEvent(BaseEvent& event, float delta)
+{
+	if (event.getTypeID() == ecs::events::TriggerSpringTrapEvent::typeID)
+	{
+		TypeID id = dynamic_cast<TriggerSpringTrapEvent*>(&event)->unitID;
+
+		// So they cant run and mess up the flight to the next tile
+		components::DynamicMovementComponent* p_movement_comp = getComponentFromKnownEntity<DynamicMovementComponent>(id);
+		p_movement_comp->mMaxVelocity = 0.0f;
+		
+		GridProp* p_gp = GridProp::GetInstance();
+
+
+		// Loop over random tiles until a valid spot is found
+		TransformComponent* start_trans_comp;
+		TransformComponent* target_trans_comp;
+		int2 area_size = p_gp->GetSize();
+
+		int rand_x = (rand() % (area_size.x / 2)) + (area_size.x / 4);
+		int rand_y = (rand() % (area_size.y / 2)) + (area_size.y / 4);
+		while (!p_gp->mGrid[rand_x][rand_y].isPassable)
+		{
+			rand_x = (rand() % (area_size.x / 2)) + (area_size.x / 4);
+			rand_y = (rand() % (area_size.y / 2)) + (area_size.y / 4);
+		}
+
+		target_trans_comp = getComponentFromKnownEntity<TransformComponent>(p_gp->mGrid[rand_x][rand_y].Id);
+
+		// units transformcomponent
+		start_trans_comp = getComponentFromKnownEntity<TransformComponent>(id);
+		
+
+		XMFLOAT2 flight_direction;
+		flight_direction.x = target_trans_comp->position.x - start_trans_comp->position.x;
+		flight_direction.y = target_trans_comp->position.y - start_trans_comp->position.y;
+
+		float dist = sqrtf(flight_direction.x * flight_direction.x + flight_direction.y * flight_direction.y);
+
+		flight_direction.x /= dist;
+		flight_direction.y /= dist;
+
+		// Make the unit jump a litte, fire is hot and so am I
+		ForceImpulseEvent knockback;
+		knockback.mDirection = DirectX::XMFLOAT3(0.0f, 1.0f, 0.0f);
+		knockback.mForce = mKnockback;
+		knockback.mEntityID = id;
+		createEvent(knockback);
+
 	}
 }
--- Game/GameMain/gameTraps/TrapSystems.h
+++ Game/GameMain/gameTraps/TrapSystems.h
@@ -21,5 +21,18 @@ namespace ecs
 			const float mDamage = 40.0f;
 			const float mKnockback = 80.0f;
 		};
+
+
+		// Starts the game, launches the correct phase after reading event
+		class SpringTrapEventSystem : public ecs::ECSSystem<SpringTrapEventSystem>
+		{
+		public:
+			SpringTrapEventSystem();
+			~SpringTrapEventSystem();
+			void readEvent(BaseEvent& event, float delta) override;
+		private:
+
+			const float mKnockback = 400.0f;
+		};
 	}
 }
