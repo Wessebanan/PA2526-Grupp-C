73755472360e42b24e33a7facf36966a9ab8607e
Wessebanan <robin_westerlund@hotmail.com>
2019-11-13 13:40:43 +0100

    Previous commit did not build oopsie. Made an independent BoundingCylinder struct that Cylinder inherits from to not confuse the compiler.

--- Physics/Physics/BoundingVolume.cpp
+++ Physics/Physics/BoundingVolume.cpp
@@ -1,174 +1,13 @@
 #include "includes\BoundingVolume.h"
 
-
-bool Sphere::Intersects(BoundingVolume* other)
-{
-	// Check which bounding volume 'other' is and test.
-	OBB* obb = dynamic_cast<OBB*>(other);
-	if (obb)
-	{
-		return BoundingSphere::Intersects(*(BoundingOrientedBox*)obb);
-	}
-	AABB* aabb = dynamic_cast<AABB*>(other);
-	if (aabb)
-	{
-		return BoundingSphere::Intersects(*(BoundingBox*)aabb);
-	}
-	Sphere* sphere = dynamic_cast<Sphere*>(other);
-	if (sphere)
-	{
-		return BoundingSphere::Intersects(*(Sphere*)sphere);
-	}
-	Cylinder* cylinder = dynamic_cast<Cylinder*>(other);
-	if (cylinder)
-	{
-		return Intersects(cylinder);
-	}
-	return false;
-}
-
-void Sphere::Transform(XMMATRIX transform)
-{
-	BoundingSphere::Transform(*(BoundingSphere*)this, transform);
-}
-
-XMFLOAT3 Sphere::GetCenter()
-{
-	return Center;
-}
-
-bool Sphere::Intersects(Cylinder& cylinder)
-{
-	return cylinder.Intersects(this);
-}
-
-bool OBB::Intersects(BoundingVolume* other)
-{
-	// Check which bounding volume 'other' is and test.
-	OBB* obb = dynamic_cast<OBB*>(other);
-	if (obb)
-	{
-		return BoundingOrientedBox::Intersects(*(BoundingOrientedBox*)obb);
-	}
-	AABB* aabb = dynamic_cast<AABB*>(other);
-	if (aabb)
-	{
-		return BoundingOrientedBox::Intersects(*(BoundingBox*)aabb);
-	}
-	Sphere* sphere = dynamic_cast<Sphere*>(other);
-	if (sphere)
-	{
-		return BoundingOrientedBox::Intersects(*(Sphere*)sphere);
-	}
-	Cylinder* cylinder = dynamic_cast<Cylinder*>(other);
-	if (cylinder)
-	{
-		return Intersects(cylinder);
-	}
-	return false;
-}
-
-void OBB::Transform(XMMATRIX transform)
-{
-	BoundingOrientedBox::Transform(*(BoundingOrientedBox*)this, transform);
-}
-
-XMFLOAT3 OBB::GetCenter()
-{
-	return Center;
-}
-
-bool OBB::Intersects(Cylinder& cylinder)
-{
-	return cylinder.Intersects(this);
-}
-
-bool AABB::Intersects(BoundingVolume* other)
-{
-	// Check which bounding volume 'other' is and test.
-	OBB* obb = dynamic_cast<OBB*>(other);
-	if (obb)
-	{
-		return BoundingBox::Intersects(*(BoundingOrientedBox*)obb);
-	}
-	AABB* aabb = dynamic_cast<AABB*>(other);
-	if (aabb)
-	{
-		return BoundingBox::Intersects(*(BoundingBox*)aabb);
-	}
-	Sphere* sphere = dynamic_cast<Sphere*>(other);
-	if (sphere)
-	{
-		return BoundingBox::Intersects(*(Sphere*)sphere);
-	}
-	Cylinder* cylinder = dynamic_cast<Cylinder*>(other);
-	if (cylinder)
-	{
-		return Intersects(*cylinder);
-	}
-	return false;
-}
-
-void AABB::Transform(XMMATRIX transform)
-{
-	BoundingBox::Transform(*(BoundingBox*)this, transform);
-}
-
-XMFLOAT3 AABB::GetCenter()
-{
-	return Center;
-}
-
-bool AABB::Intersects(Cylinder& cylinder)
-{
-	return cylinder.Intersects(this);
-}
-
-bool Cylinder::Intersects(BoundingVolume* other)
-{
-	// Check which bounding volume 'other' is and test.
-	OBB* obb = dynamic_cast<OBB*>(other);
-	if (obb)
-	{
-		return Intersects(*(BoundingOrientedBox*)obb);
-	}
-	AABB* aabb = dynamic_cast<AABB*>(other);
-	if (aabb)
-	{
-		return Intersects(*(BoundingBox*)aabb);
-	}
-	Sphere* sphere = dynamic_cast<Sphere*>(other);
-	if (sphere)
-	{
-		return Intersects(*(Sphere*)sphere);
-	}
-	Cylinder* cylinder = dynamic_cast<Cylinder*>(other);
-	if (cylinder)
-	{
-		return Intersects(*cylinder);
-	}
-	return false;
-}
-
-void Cylinder::Transform(XMMATRIX transform)
-{ 
-	// Only does translation because only tiles use this.
-	XMStoreFloat3(&mCenter, XMVector3Transform(XMLoadFloat3(&mCenter), transform));
-}
-
-XMFLOAT3 Cylinder::GetCenter()
-{
-	return mCenter;
-}
-
-void Cylinder::CreateFromTile(XMFLOAT3 position, float height, float radius)
+void BoundingCylinder::CreateFromTile(XMFLOAT3 position, float height, float radius)
 {
 	mExtentsY = height / 2.0f;
 	mRadius = radius * HALF_SQRT_3;
 	mCenter = XMFLOAT3(position.x, position.y - (height / 2.0f), position.z);
 }
 
-bool Cylinder::Intersects(BoundingBox& aabb)
+bool BoundingCylinder::Intersects(BoundingBox& aabb)
 {
 	// Immediately return if aabb is above or below cylinder.
 	if (aabb.Center.y - aabb.Extents.y > mCenter.y + mExtentsY ||
@@ -188,7 +27,7 @@ bool Cylinder::Intersects(BoundingBox& aabb)
 	return x_z_dist_to_center < mRadius;
 }
 
-bool Cylinder::Intersects(BoundingSphere& sphere)
+bool BoundingCylinder::Intersects(BoundingSphere& sphere)
 {
 	// Immediately return if sphere is above or below cylinder.
 	if (sphere.Center.y - sphere.Radius > mCenter.y + mExtentsY ||
@@ -205,7 +44,7 @@ bool Cylinder::Intersects(BoundingSphere& sphere)
 	return x_z_center_to_center_dist < sphere.Radius + mRadius;
 }
 
-bool Cylinder::Intersects(BoundingOrientedBox& obb)
+bool BoundingCylinder::Intersects(BoundingOrientedBox& obb)
 {
 	// Only corner intersection, edges are hard af.
 	XMFLOAT3* corners = new XMFLOAT3[obb.CORNER_COUNT];
@@ -232,7 +71,7 @@ bool Cylinder::Intersects(BoundingOrientedBox& obb)
 	return false;
 }
 
-bool Cylinder::Intersects(Cylinder& cylinder)
+bool BoundingCylinder::Intersects(BoundingCylinder& cylinder)
 {
 	// Return if above or below.
 	if (cylinder.mCenter.y - cylinder.mExtentsY > mCenter.y + mExtentsY ||
@@ -248,3 +87,149 @@ bool Cylinder::Intersects(Cylinder& cylinder)
 
 	return x_z_center_to_center_dist < cylinder.mRadius + mRadius;
 }
+
+bool Sphere::Intersects(BoundingVolume* other)
+{
+	// Check which bounding volume 'other' is and test.
+	OBB* obb = dynamic_cast<OBB*>(other);
+	if (obb)
+	{
+		return BoundingSphere::Intersects(*(BoundingOrientedBox*)obb);
+	}
+	AABB* aabb = dynamic_cast<AABB*>(other);
+	if (aabb)
+	{
+		return BoundingSphere::Intersects(*(BoundingBox*)aabb);
+	}
+	Sphere* sphere = dynamic_cast<Sphere*>(other);
+	if (sphere)
+	{
+		return BoundingSphere::Intersects(*(Sphere*)sphere);
+	}
+	Cylinder* cylinder = dynamic_cast<Cylinder*>(other);
+	if (cylinder)
+	{
+		return cylinder->BoundingCylinder::Intersects(*(BoundingSphere*)this);
+	}
+	return false;
+}
+
+void Sphere::Transform(XMMATRIX transform)
+{
+	BoundingSphere::Transform(*(BoundingSphere*)this, transform);
+}
+
+XMFLOAT3 Sphere::GetCenter()
+{
+	return Center;
+}
+
+bool OBB::Intersects(BoundingVolume* other)
+{
+	// Check which bounding volume 'other' is and test.
+	OBB* obb = dynamic_cast<OBB*>(other);
+	if (obb)
+	{
+		return BoundingOrientedBox::Intersects(*(BoundingOrientedBox*)obb);
+	}
+	AABB* aabb = dynamic_cast<AABB*>(other);
+	if (aabb)
+	{
+		return BoundingOrientedBox::Intersects(*(BoundingBox*)aabb);
+	}
+	Sphere* sphere = dynamic_cast<Sphere*>(other);
+	if (sphere)
+	{
+		return BoundingOrientedBox::Intersects(*(Sphere*)sphere);
+	}
+	Cylinder* cylinder = dynamic_cast<Cylinder*>(other);
+	if (cylinder)
+	{
+		return cylinder->BoundingCylinder::Intersects(*(BoundingOrientedBox*)this);
+	}
+	return false;
+}
+
+void OBB::Transform(XMMATRIX transform)
+{
+	BoundingOrientedBox::Transform(*(BoundingOrientedBox*)this, transform);
+}
+
+XMFLOAT3 OBB::GetCenter()
+{
+	return Center;
+}
+
+bool AABB::Intersects(BoundingVolume* other)
+{
+	// Check which bounding volume 'other' is and test.
+	OBB* obb = dynamic_cast<OBB*>(other);
+	if (obb)
+	{
+		return BoundingBox::Intersects(*(BoundingOrientedBox*)obb);
+	}
+	AABB* aabb = dynamic_cast<AABB*>(other);
+	if (aabb)
+	{
+		return BoundingBox::Intersects(*(BoundingBox*)aabb);
+	}
+	Sphere* sphere = dynamic_cast<Sphere*>(other);
+	if (sphere)
+	{
+		return BoundingBox::Intersects(*(Sphere*)sphere);
+	}
+	Cylinder* cylinder = dynamic_cast<Cylinder*>(other);
+	if (cylinder)
+	{
+		return cylinder->BoundingCylinder::Intersects(*(BoundingBox*)this);
+	}
+	return false;
+}
+
+void AABB::Transform(XMMATRIX transform)
+{
+	BoundingBox::Transform(*(BoundingBox*)this, transform);
+}
+
+XMFLOAT3 AABB::GetCenter()
+{
+	return Center;
+}
+
+bool Cylinder::Intersects(BoundingVolume* other)
+{
+	// Check which bounding volume 'other' is and test.
+	OBB* obb = dynamic_cast<OBB*>(other);
+	if (obb)
+	{
+		return BoundingCylinder::Intersects(*(BoundingOrientedBox*)obb);
+	}
+	AABB* aabb = dynamic_cast<AABB*>(other);
+	if (aabb)
+	{
+		return BoundingCylinder::Intersects(*(BoundingBox*)aabb);
+	}
+	Sphere* sphere = dynamic_cast<Sphere*>(other);
+	if (sphere)
+	{
+		return BoundingCylinder::Intersects(*(Sphere*)sphere);
+	}
+	Cylinder* cylinder = dynamic_cast<Cylinder*>(other);
+	if (cylinder)
+	{
+		return BoundingCylinder::Intersects(*cylinder);
+	}
+	return false;
+}
+
+void Cylinder::Transform(XMMATRIX transform)
+{ 
+	// Only does translation because only tiles use this.
+	XMStoreFloat3(&mCenter, XMVector3Transform(XMLoadFloat3(&mCenter), transform));
+}
+
+XMFLOAT3 Cylinder::GetCenter()
+{
+	return mCenter;
+}
+
--- Physics/Physics/includes/BoundingVolume.h
+++ Physics/Physics/includes/BoundingVolume.h
@@ -5,8 +5,25 @@
 
 #define HALF_SQRT_3 0.866f
 
-// Base struct for unspecified bounding volumes.
 using namespace DirectX;
+
+struct BoundingCylinder
+{
+	XMFLOAT3 mCenter;
+	float mExtentsY; // Half height.
+	float mRadius;
+
+	// Radius is center to corner
+	void CreateFromTile(XMFLOAT3 position, float height, float radius = 1.0f);
+
+	bool Intersects(BoundingBox& aabb);
+	bool Intersects(BoundingSphere& sphere);
+	bool Intersects(BoundingOrientedBox& obb);
+	bool Intersects(BoundingCylinder& cylinder);
+};
+
+
+// Base struct for unspecified bounding volumes.
 struct BoundingVolume
 {
 	virtual ~BoundingVolume() = default;
@@ -16,49 +33,37 @@ struct BoundingVolume
 };
 
 //Empty struct inheriting from BoundingVolume (base class) and DirectX::BoundingSphere.
-struct Sphere : public BoundingVolume, DirectX::BoundingSphere 
+struct Sphere : public BoundingVolume, BoundingSphere 
 {
 	bool Intersects(BoundingVolume* other);
 	void Transform(XMMATRIX transform);
 	XMFLOAT3 GetCenter();
-private:
-	bool Intersects(Cylinder& cylinder);
+//private:
+//	bool Intersects(Cylinder& cylinder);
 };
 //Empty struct inheriting from BoundingVolume (base class) and DirectX::BoundingOrientedBox.
-struct OBB : public BoundingVolume, DirectX::BoundingOrientedBox 
+struct OBB : public BoundingVolume, BoundingOrientedBox 
 {
 	bool Intersects(BoundingVolume* other);
 	void Transform(XMMATRIX transform);
 	XMFLOAT3 GetCenter();
-private:
-	bool Intersects(Cylinder& cylinder);
+//private:
+//	bool Intersects(Cylinder& cylinder);
 };
 //Empty struct inheriting from BoundingVolume (base class) and DirectX::BoundingBox.
-struct AABB : public BoundingVolume, DirectX::BoundingBox 
+struct AABB : public BoundingVolume, BoundingBox 
 {
 	bool Intersects(BoundingVolume* other);
 	void Transform(XMMATRIX transform);
 	XMFLOAT3 GetCenter();
-private:
-	bool Intersects(Cylinder& cylinder);
+//private:
+//	bool Intersects(Cylinder& cylinder);
 };
 
 //Axis aligned so that y is height, DO NOT ROTATE, specifically designed for tiles. 
-struct Cylinder : public BoundingVolume
+struct Cylinder : public BoundingVolume, BoundingCylinder
 {
 	bool Intersects(BoundingVolume* other);
 	void Transform(XMMATRIX transform);
 	XMFLOAT3 GetCenter();
-
-	// Radius is center to corner
-	void CreateFromTile(XMFLOAT3 position, float height, float radius = 1.0f);
-private:
-	XMFLOAT3 mCenter;
-	float mExtentsY; // Half height.
-	float mRadius;
-
-	bool Intersects(BoundingBox& aabb);
-	bool Intersects(BoundingSphere& sphere);
-	bool Intersects(BoundingOrientedBox& obb);
-	bool Intersects(Cylinder& cylinder);
 };
