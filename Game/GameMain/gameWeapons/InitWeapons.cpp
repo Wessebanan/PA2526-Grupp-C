f57c126bad9d1509fbd95a801f3c4865df8f0cfe
Marcus Holmberg <mackan.holmberg@hotmail.com>
2019-11-19 09:27:30 +0100

    Removed line that conflicted compilation.

--- Game/GameMain/gameWeapons/InitWeapons.cpp
+++ Game/GameMain/gameWeapons/InitWeapons.cpp
@@ -7,7 +7,5 @@ using namespace ecs;
 void InitWeapons(EntityComponentSystem& rEcs)
 {
 	rEcs.reserveComponentCount<components::WeaponComponent>(400);
-	rEcs.reserveComponentCount<components::MeshComponent>(400);
-
 	rEcs.createSystem<systems::WeaponSpawner>(5);
 }
