c4be809e26e5e5898fe5065c03bc6f337812d0a2
Mabaria <magnomrikard@gmail.com>
2019-11-21 13:39:53 +0100

    Make donut more donuty

--- Game/GameMain/gameWeapons/WeaponSpawner.h
+++ Game/GameMain/gameWeapons/WeaponSpawner.h
@@ -44,8 +44,8 @@ namespace ecs
 		private:
 
 			float mSpawnTimer;
-			const float SPAWN_OUTER_RADIUS = 7.f;
-			const float SPAWN_INNER_RADIUS = 3.f;
+			const float SPAWN_OUTER_RADIUS = 8.f;
+			const float SPAWN_INNER_RADIUS = 5.f;
 			const float COOLDOWN_BASE_TIME = 9;
 			const int COOLDOWN_TIME_VARIANCE = 3;
 
