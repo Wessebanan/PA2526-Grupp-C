5b99ef6a6ea76631e32416a78fca4576eef936f1
Marcus Holmberg <mackan.holmberg@hotmail.com>
2019-11-29 08:39:01 +0100

    Added component flag enum.

--- ECS/plainECS/includes/ecsGlobals.h
+++ ECS/plainECS/includes/ecsGlobals.h
@@ -6,3 +6,9 @@ typedef unsigned int ID;
 typedef unsigned int TypeID;
 
 enum MEMORY_DESC_TYPE { MEMORY_DESC_TYPE_UNKOWN, MEMORY_DESC_TYPE_SIZE, MEMORY_DESC_TYPE_COUNT };
+
+typedef char COMP_FLAG;
+enum COMP_FLAGS
+{
+	COMP_FLAG_ALIVE = 1
+};
