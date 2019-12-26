a639dcc13e24169687cd88cc5e4048b6feca165c
Lhure <maxjonilsson@gmail.com>
2019-12-26 17:31:44 +0100

    Rebuilt for nerw VS

--- Game/GameMain/InitHttpServer.h
+++ Game/GameMain/InitHttpServer.h
@@ -12,7 +12,8 @@ void InitHttpServer(ecs::EntityComponentSystem& rECS)
 	std::string text_str;
 	HttpServer::GetLocalIp4(text_str);
 	std::wstring_convert<std::codecvt<wchar_t, char, std::mbstate_t>> convert;
-	std::wstring text_wstr = convert.from_bytes(text_str);
+	//std::wstring text_wstr = convert.from_bytes(text_str);
+	std::wstring text_wstr = L"192.168.1.244";
 
 	text_wstr.insert(0, L"Join at adress: ");
 	text.mStrText = text_wstr;
